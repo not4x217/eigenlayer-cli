60a37e7b377dd4aefac01238c0cf0b01a7b0699b
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-01-17 21:28:17 +0800

    fix addresses

--- pkg/internal/common/helper.go
+++ pkg/internal/common/helper.go
@@ -65,9 +65,9 @@ var ChainMetadataMap = map[int64]types.ChainMetadata{
 	},
 	BuilderPlaygroundChainId: {
 		BlockExplorerUrl:            "",
-		ELDelegationManagerAddress:  "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
-		ELAVSDirectoryAddress:       "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
-		ELRewardsCoordinatorAddress: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+		ELDelegationManagerAddress:  "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+		ELAVSDirectoryAddress:       "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+		ELRewardsCoordinatorAddress: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
 		WebAppUrl:                   "",
 		ProofStoreBaseURL:           "",
 	},
